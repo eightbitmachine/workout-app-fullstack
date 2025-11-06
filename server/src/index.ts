1ff40614f5a2e4fb5b1b84ab63eacb9578244568
Mario <mario@eightbitmachine.com>
2025-11-05 23:23:06 -0400

    Add server for backend
    Create a backend server using Hono

---
+++ server/src/index.ts
@@ -0,0 +1,27 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import dotenv from "dotenv";
+import health from "./health.js";
+
+const configResult = dotenv.config();
+
+const app = new Hono();
+
+app.use("*", cors());
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.route("/health", health);
+
+serve(
+  {
+    fetch: app.fetch,
+    port: parseInt(process.env.PORT),
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
