1c1270f13573b81c755d1c75d8c6940fe39410c8
Mario <mario@eightbitmachine.com>
2025-11-06 23:37:20 -0400

    Refactor auth/login handler
    Extract the login logic to a new handler function.
    
    `system` dependency is injected into the handler, along with the Hono
    context, rather than used implicitly a local global (local global lol)
    because of scope.

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,30 +1,32 @@
-import { Hono } from "hono";
+import { Hono, type Context } from "hono";
 import type { System } from "../system.js";
+
+const loginHandler = async (system: System, c: Context) => {
+  const formData = await c.req.formData();
+  const email = formData.get("email")?.toString() || "";
+  const password = formData.get("password")?.toString() || "";
+
+  const user = await system.db
+    ?.withSchema(system?.env?.POSTGRES_DB || "")
+    .selectFrom("users")
+    .select(["id", "email", "type", "password_hash"])
+    .where("email", "=", email)
+    .executeTakeFirst();
+
+  return c.json({
+    ok: true,
+    user: {
+      id: user?.id,
+      email: user?.email,
+      type: user?.type,
+    },
+  });
+};
 
 const authRoutes = (system: System) => {
   const app = new Hono();
 
-  app.post("/login", async (c) => {
-    const formData = await c.req.formData();
-    const email = formData.get("email")?.toString() || "";
-    const password = formData.get("password")?.toString() || "";
-
-    const user = await system.db
-      ?.withSchema(system?.env?.POSTGRES_DB || "")
-      .selectFrom("users")
-      .select(["id", "email", "type", "password_hash"])
-      .where("email", "=", email)
-      .executeTakeFirst();
-
-    return c.json({
-      ok: true,
-      user: {
-        id: user?.id,
-        email: user?.email,
-        type: user?.type,
-      },
-    });
-  });
+  app.post("/login", (c) => { return loginHandler(system, c); });
 
   return app;
 };
