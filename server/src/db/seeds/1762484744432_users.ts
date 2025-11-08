5422cab31a193ea2d7663a3410bfe79b6f214862
Mario <mario@eightbitmachine.com>
2025-11-07 22:44:39 -0400

    fixup! Use Argon2 to securely hash passwords

--- server/src/db/seeds/1762484744432_users.ts
+++ server/src/db/seeds/1762484744432_users.ts
@@ -3,6 +3,8 @@ import { type Database } from "../schema.js";
 import { hashPassword } from "../../auth.js";
 
 process.loadEnvFile("../.env");
+const schema = process.env.POSTGRES_DB || "";
+
 // When using kysely-ctl, it will be from the root of the `server` directory, so only one level up
 
 // replace `any` with your database interface.
@@ -10,7 +12,7 @@ export async function seed(db: Kysely<Database>): Promise<void> {
   // seed code goes here...
   // note: this function is mandatory. you must implement this function.
   await db
-    .withSchema(process.env.POSTGRES_DB || "")
+    .withSchema(schema)
     .insertInto("users")
     .values([
       {
@@ -22,7 +24,7 @@ export async function seed(db: Kysely<Database>): Promise<void> {
       },
       {
         email: "athlete@example.com",
-        password_hash: await hashPassword("athelete-987"),
+        password_hash: await hashPassword("athlete-987"),
         type: "ATHLETE",
         modified_at: new Date(),
         deleted_at: sql`NULL`,
