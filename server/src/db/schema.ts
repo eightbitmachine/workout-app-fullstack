f9408f9ad3673824bd658a2c4166449c23980f12
Mario <mario@eightbitmachine.com>
2025-11-10 16:11:04 -0400

    Create types for the DB User records

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,4 +1,12 @@
-import { type ColumnType, type Generated } from "kysely";
+import type {
+  ColumnType,
+  Generated,
+  Selectable,
+  Insertable,
+  Updateable,
+} from "kysely";
+
+// NOTE: See Kysely docs on creating types <https://kysely.dev/docs/getting-started#types> 
 
 export interface Database {
   users: UsersTable;
@@ -13,3 +21,11 @@ export interface UsersTable {
   modified_at: ColumnType<Date, Date, Date>;
   deleted_at: ColumnType<Date | null, Date, Date | null>;
 }
+
+export type User = Selectable<UsersTable>;
+export type NewUser = Insertable<UsersTable>;
+export type UserUpdate = Updateable<UsersTable>;
+
+// NOTE: Types can be generated via codegen by instrospecting the DB using
+// third-party tools like `kysely-codegen`. 
+// See <https://kysely.dev/docs/generating-types>
